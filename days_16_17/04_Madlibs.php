85b8d8213155c515028ca24be22942c840478d92
chelsielii <cl6216@nyu.edu>
2024-03-30 11:45:26 -0400

    Update 04_Madlibs.php

--- days_16_17/04_Madlibs.php
+++ days_16_17/04_Madlibs.php
@@ -6,8 +6,25 @@
   <body>
     <h1>Mad Libs</h1>
     <?php
+      if ($_POST['submit']){
+      $name = $_POST["name"];
+      $verb = $_POST['verb'];
+      $noun = $_POST['noun'];
+      $adjective = $_POST['adjective'];
+      $adverb = $_POST['adverb'];
+      print "the $name $noun was very hungry so it decided to $adverb $verb to the nearest restaurant";
+      }else {
+        print "problem"; 
+      }
+      
+  
+
     
+
      ?>
+
+
+
     <form method="post" action="04_Madlibs.php">
       Name: <input type="text" name="name"><br>
       Verb: <input type="text" name="verb"><br>
@@ -16,6 +33,10 @@
       Adverb: <input type="text" name="adverb"><br>
       <input type="submit">
     </form>
+  
+
+
+
 
   </body>
 </html>
