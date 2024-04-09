25e00ef0d8db26efdd3316771c577f3acd1620b8
Emily Zhao <35831739+emilydidthis@users.noreply.github.com>
2024-04-09 15:16:10 -0400

    database starter files

---
+++ days_20_21/inclass/to_do_list.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+  <head>
+    <title>To Do List</title>
+  </head>
+
+  <body>
+    <h1>To Do List</h1>
+
+    <form method="POST" action="save.php">
+      <p>Create New Item:</p>
+      <input type="text" name="title">
+      <input type="submit">
+    </form>
+
+
+    <h3>Your Items</h3>
+    <ul>
+    
+   </ul>
+
+
+  </body>
+</html>
